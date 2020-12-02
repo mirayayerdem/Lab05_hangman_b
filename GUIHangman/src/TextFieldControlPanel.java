ed85390e4b3e4c9c3a5641b1f7116af9583b9311
miray <mirayayerdem@gmail.com>
2020-12-02 22:29:06 +0300

    TextFiledControlPanel Class has been modified

--- GUIHangman/src/TextFieldControlPanel.java
+++ GUIHangman/src/TextFieldControlPanel.java
@@ -6,11 +6,20 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * TextFieldControlPanel
+ *
+ * @author Miray Ayerdem
+ * @version  2020/12/2
+ */
+
 public class TextFieldControlPanel extends JPanel {
+    //instances
     Hangman hangman;
     JTextField txt;
     ActionListener listener;
 
+    //constructor
     public TextFieldControlPanel(Hangman hangman)
     {
         this.hangman = hangman;
@@ -20,17 +29,19 @@ public class TextFieldControlPanel extends JPanel {
 
         }
 
-    class MyActionListener implements ActionListener {
+    class MyActionListener implements ActionListener {  //listener class
+
         @Override
-        public void actionPerformed(ActionEvent e) {
+        public void actionPerformed(ActionEvent e)
+        {
             String letter;
             String letters = txt.getText();
             letter = "";
-            for(int i = 0 ; i < letters.length() ; i++)
+            for(int i = 0 ; i < letters.length() ; i++) //when the button is tapped, call the tryThis method
             {
                 hangman.tryThis(letters.charAt(i));
             }
-            txt.setText("");
+            txt.setText(""); //set the empty text
         }
     }
 
