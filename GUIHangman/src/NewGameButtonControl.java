27131c6f073cdc79d0a3c564f6976233f0269f1c
miray <mirayayerdem@gmail.com>
2020-12-02 22:31:48 +0300

    NewGameButtonControl has been modified

--- GUIHangman/src/NewGameButtonControl.java
+++ GUIHangman/src/NewGameButtonControl.java
@@ -1,26 +1,46 @@
 package GUIHangman.src;
 
+import ConsoleHangman.src.IHangmanView;
 import cs102.Hangman;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class NewGameButtonControl extends JButton
+/**
+ * NewGameButtonControl
+ *
+ * @author Miray Ayerdem
+ * @version  2020/12/2
+ */
+
+public class NewGameButtonControl extends JButton implements IHangmanView
 {
+    //instances
     Hangman hangman;
+
+    //constructor
     public NewGameButtonControl(Hangman hangman)
     {
         this.hangman = hangman;
         this.setText("New Game");
+        this.setEnabled(false);
         this.addActionListener(new ButtonListener());
     }
-    class ButtonListener implements ActionListener
+
+    @Override
+    public void updateView(Hangman hangmanModel)
+    {
+        if(hangman.isGameOver()) //all buttons are enabled
+            this.setEnabled(true);
+    }
+
+    class ButtonListener implements ActionListener //new game has been started
     {
         @Override
         public void actionPerformed(ActionEvent e)
         {
-           hangman.initNewGame();
+            hangman.initNewGame();
         }
     }
 }
