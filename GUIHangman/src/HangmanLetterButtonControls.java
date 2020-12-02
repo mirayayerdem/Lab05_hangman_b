ac4e7088526034a089d90694114384aa6d23f3ec
miray <mirayayerdem@gmail.com>
2020-12-02 22:53:06 +0300

    HangmanLetterButtonControls Class is added

---
+++ GUIHangman/src/HangmanLetterButtonControls.java
@@ -0,0 +1,37 @@
+package GUIHangman.src;
+
+import ConsoleHangman.src.IHangmanView;
+import cs102.Hangman;
+/**
+ * HangmanLetterButtonsControls
+ *
+ * @author Miray Ayerdem
+ * @version  2020/12/2
+ */
+
+public class HangmanLetterButtonControls extends LetterButtonControls implements IHangmanView {
+    private String letters;
+    //constructors
+    public HangmanLetterButtonControls(String letters)
+    {
+        super(letters);
+    }
+    public HangmanLetterButtonControls(String letters, int row, int column)
+    {
+        super(letters,row,column);
+    }
+
+    @Override
+    public void updateView(Hangman hangmanModel)
+    {
+        setDisabled( hangmanModel.getUsedLetters());
+        if ( hangmanModel.isGameOver() ) //if game is over, all buttons are disabled
+        {
+            setEnabledAll( false);
+        }
+        if ( hangmanModel.getUsedLetters().length() == 0 ) //if the game is news, all buttons are enabled
+        {
+            setEnabledAll(true);
+        }
+    }
+}
